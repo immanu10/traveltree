85e9e043936f50868a19719c4e65024b226043ed
immanu10 <54578417+immanu10@users.noreply.github.com>
2023-12-05 21:39:46 +0530

    added form example

--- app/(home)/explore/page.tsx
+++ app/(home)/explore/page.tsx
@@ -1,9 +1,12 @@
+import { UserNameForm } from "@/components/form/username";
 import { Button } from "@/components/ui/button";
 import { XIcon } from "lucide-react";
 
 export default function ExplorePage() {
   return (
     <div className="">
+      <p>Update your Username to create tree</p>
+      <UserNameForm />
       <div className="flex items-center rounded-lg bg-gray-100 px-3 py-1 text-sm font-normal my-2">
         Explore travel post from community and Like them to add to your bucket
         list.
