a6063247da5e791095c7fe3f229c305fa22b856f
immanu10 <54578417+immanu10@users.noreply.github.com>
2024-01-23 19:36:37 +0530

    add feedcard to explore page

--- app/(home)/explore/page.tsx
+++ app/(home)/explore/page.tsx
@@ -1,22 +1,27 @@
 import { FeedCard } from "@/components/feed-card";
+import { createClient } from "@/lib/supabase/server";
+import { cookies } from "next/headers";
 
-export default function ExplorePage() {
+export default async function ExplorePage() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data } = await supabase
+    .from("posts")
+    .select(`*,profiles("username")`)
+    .order("inserted_at", { ascending: false });
+
   return (
     <div className="">
-      {/* <div className="border-b py-1 px-2">
+      <div className="border-b py-1 px-2">
         <div className="flex items-center rounded-lg bg-gray-100 px-3 py-1 text-sm font-normal my-2">
           Explore travel post from the community and Like them to add to your
           bucket list.
         </div>
-      </div> */}
-      <FeedCard />
-      <FeedCard />
-      <FeedCard />
-      <FeedCard />
-      <FeedCard />
-      <FeedCard />
-      <FeedCard />
-      <FeedCard />
+      </div>
+      {data?.map((item) => {
+        return <FeedCard key={item.id} data={item} />;
+      })}
     </div>
   );
 }
--- components/feed-card.tsx
+++ components/feed-card.tsx
@@ -2,31 +2,39 @@ import { CopyIcon, DotIcon, MapIcon, MountainIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
+import { Database } from "@/lib/supabase/types";
 
-export function FeedCard() {
+type FeedCardData = Database["public"]["Tables"]["posts"]["Row"] & {
+  profiles: { username: string | null } | null;
+};
+
+export function FeedCard({ data }: { data: FeedCardData }) {
+  const { profiles, title, description, map_url, best_months, inserted_at } =
+    data;
+
   return (
     <div className="border-b py-3 flex flex-col gap-4">
       <div className="px-5">
         <p className="inline-flex items-center text-sm  text-gray-500">
           <span>Posted by</span>
-          <Link href="/manoj" className="ml-1 hover:underline">
-            @manoj
+          <Link
+            href={`/${profiles?.username}`}
+            className="ml-1 hover:underline"
+          >
+            {profiles?.username}
           </Link>
         </p>
-        <h1 className="font-medium text-lg">Kodachadri, heaven on earth</h1>
+        <h1 className="font-medium text-lg">{title}</h1>
         <div className="flex space-x-2 text-sm  text-gray-500 items-center">
           <p>Karnatak, India</p>
           <DotIcon className="w-3 h-3" />
-          <p>Best time to visit: Apr, May</p>
+          <p>{`Best time to visit: ${best_months?.toString()}`}</p>
         </div>
         <div className="my-2 py-2 rounded-sm flex space-x-2 h-8 items-center text-xs">
           <MapIcon className="w-3 h-3 text-blue-500" />
           {/* <Separator orientation="vertical" /> */}
-          <Link
-            href="https://maps.app.goo.gl/gyagcBYHCd77PxgH7"
-            className="text-blue-500 hover:underline"
-          >
-            https://maps.app.goo.gl/gyagcBYHCd77PxgH7
+          <Link href={map_url!} className="text-blue-500 hover:underline">
+            {map_url}
           </Link>
           <Separator orientation="vertical" />
 
@@ -35,12 +43,7 @@ export function FeedCard() {
           </Button>
         </div>
         <div className="text-sm my-1 w-full">
-          <p className="">
-            Kodachadri is a mountain peak with dense forests in the Western
-            Ghats in South India, 78 km from Shimoga. book dmfdfml orler mer
-            dmfdf dfdf mkk sss trek morning at 4am mmdfmdf mm mdfmdf dshimomd m
-            loreme...more
-          </p>
+          <p className="">{description}</p>
         </div>
       </div>
       <div
