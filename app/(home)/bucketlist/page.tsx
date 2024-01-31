f8c2244a109c16faef8d751cbb8be811fd3177b9
immanu10 <54578417+immanu10@users.noreply.github.com>
2024-01-31 16:16:59 +0530

    bucketlist page ui wip

--- app/(home)/bucketlist/page.tsx
+++ app/(home)/bucketlist/page.tsx
@@ -1,7 +1,16 @@
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { createClient } from "@/lib/supabase/server";
 import { cn } from "@/lib/utils";
+import { MoreHorizontalIcon, MountainIcon } from "lucide-react";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
@@ -13,8 +22,6 @@ export default async function Page({
   params: { username: string };
   searchParams: { status?: string };
 }) {
-  console.log({ params, searchParams });
-
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
   const {
@@ -25,56 +32,94 @@ export default async function Page({
     redirect("/");
   }
 
-  const { data } = await supabase
-    .from("bucketlists")
-    .select("*,posts(*)")
-    .eq("user_id", session.user.id);
+  // const { data } = await supabase
+  //   .from("bucketlists")
+  //   .select("*,posts(*)")
+  //   .eq("user_id", session.user.id);
 
-  if (!data) {
-    return <p className="text-red-500">No Bucketlist Added</p>;
-  }
+  // if (!data) {
+  //   return <p className="text-red-500">No Bucketlist Added</p>;
+  // }
 
   return (
     <div className="my-4">
-      {/* <h1>BucketList - {searchParams.status}</h1>
-      <nav className={cn("flex space-x-2")}>
-        {["todo", "visited"].map((item, i) => (
-          <Link
-            key={i}
-            href={`/${params.username}/bucketlist?status=${item}`}
-            className={cn(
-              buttonVariants({ variant: "ghost" }),
-              // pathname === item.href
-              // ?
-              // "bg-muted hover:bg-muted",
-              // :
-              "hover:bg-transparent hover:underline",
-              "justify-start"
-            )}
-          >
-            {item}
-          </Link>
-        ))}
-      </nav> */}
       <div className="border-l-4 border-gray-300 px-2">
         <h1 className="text-xl">Bucketlist</h1>
       </div>
-      <div className="mt-4">
-        {data.map((item) => {
-          return (
-            <div key={item.id}>
-              <p>{item.posts?.title}</p>
-              <a
-                href={item.posts?.map_url!}
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:underline text-xs"
-              >
-                {item.posts?.map_url}
-              </a>
+      <div className="mt-4 w-full grid grid-cols-3 gap-4 text-xs text-muted-foreground justify-items-center">
+        <p>Travel Post</p>
+        <p>Visited on</p>
+        <p>Action</p>
+      </div>
+      <div className="mt-4 space-y-2">
+        <div className="todo-section">
+          <div className="border-l-4 border-green-500 px-2">
+            <h2>Todo</h2>
+          </div>
+          <div className="py-2 border-b grid grid-cols-3 gap-4 items-center justify-items-center">
+            <div className="travel-post">
+              <div className="flex space-x-2">
+                <form action="">
+                  <button
+                    type="submit"
+                    className="px-5 flex flex-col cursor-pointer group transition-colors w-fit"
+                  >
+                    <div className="w-[34px] h-[34px] flex items-center justify-center rounded-full group-hover:bg-pink-500/10">
+                      <MountainIcon
+                        className={cn(
+                          "text-gray-400 w-[18px] h-[18px] group-hover:text-pink-500"
+                          // {
+                          //   "fill-pink-500 text-pink-500":
+                          //     optimisticLikeState.likedByCurrentUser,
+                          // }
+                        )}
+                      />
+                    </div>
+                    <div className="text-xs text-gray-400 group-hover:text-pink-500">
+                      <span>{`1.2 K`}</span>
+                    </div>
+                  </button>
+                </form>
+                <div>
+                  <p className="inline-flex items-center text-xs  text-gray-500">
+                    <span>Posted by </span>
+                    <Link href={`/${"manoj"}`} className="ml-1 hover:underline">
+                      {"manoj"}
+                    </Link>
+                  </p>
+                  <Link href={"/post"}>
+                    <p className="text-sm">Kodachadri, hill station</p>
+                  </Link>
+                </div>
+              </div>
             </div>
-          );
-        })}
+            <div className="visited-on">
+              <p className="text-sm">October,2023</p>
+            </div>
+            <div className="action">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                  >
+                    <MoreHorizontalIcon className="h-4 w-4" />
+                    <span className="sr-only">Open menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-[160px]">
+                  <DropdownMenuItem>Delete Bucketlist</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </div>
+        <div className="visited-section">
+          <div className="border-l-4 border-blue-500 px-2">
+            <h2>Visited</h2>
+          </div>
+          <div className="grid grid-cols-3"></div>
+        </div>
       </div>
     </div>
   );
