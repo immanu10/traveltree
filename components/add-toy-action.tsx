f02807b4eb3b2155c7a0340270af6f934deac619
immanu10 <54578417+immanu10@users.noreply.github.com>
2024-02-22 19:38:24 +0530

    wip: buy slot

--- components/add-toy-action.tsx
+++ components/add-toy-action.tsx
@@ -11,6 +11,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
+import { BuyCoffeeDialog } from "./buy-coffee-dialog";
 
 export async function AddToyAction({
   userId,
@@ -33,39 +34,21 @@ export async function AddToyAction({
       {totalAddToySlot > 0 ? (
         <AddToy slotCount={totalAddToySlot} />
       ) : (
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline" size="sm">
-              <Plus className="h-4 w-4 mr-2" />
-              Add
-              <span className="ml-1  text-xs text-muted-foreground">
-                0 slots left
-              </span>
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>0 slots left</DialogTitle>
-              <DialogDescription>
-                {"You've used all your slots for adding toys."}
-              </DialogDescription>
-            </DialogHeader>
-            <div className="flex flex-col items-center gap-2">
-              <h2 className="font-medium text-center">Get it for</h2>
-              <div className="text-center">
-                <h4 className="text-6xl font-bold">$1</h4>
-                <p className="text-sm font-medium text-muted-foreground">
-                  Per slot
-                </p>
-              </div>
-            </div>
-            <Button className="">Buy now</Button>
-          </DialogContent>
-        </Dialog>
+        <BuyCoffeeDialog>
+          <Button variant="outline" size="sm">
+            <Plus className="h-4 w-4 mr-2" />
+            Add
+            <span className="ml-1  text-xs text-muted-foreground">
+              0 slots left
+            </span>
+          </Button>
+        </BuyCoffeeDialog>
       )}
-      <Button className="" size="sm">
-        Buy more
-      </Button>
+      <BuyCoffeeDialog>
+        <Button className="" size="sm">
+          Buy more
+        </Button>
+      </BuyCoffeeDialog>
     </div>
   );
 }
---
+++ components/buy-coffee-dialog.tsx
@@ -0,0 +1,49 @@
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+
+export function BuyCoffeeDialog({ children }: { children: React.ReactNode }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Need more slots?</DialogTitle>
+          <DialogDescription>
+            {"You can support me and get slots for your toys."}
+          </DialogDescription>
+        </DialogHeader>
+        <div className="font-medium text-sm flex flex-col items-center">
+          <p>You buy me a coffee</p>
+          <p>You get slots for your toys</p>
+        </div>
+        <div>
+          <div className="flex justify-center  gap-6 px-4 py-2 rounded-full border border-border text-4xl">
+            <div className="flex space-x-2 items-center">
+              <p className="font-semibold">1</p>
+              <p className="text-2xl text-muted-foreground font-medium">x</p>
+              <p className="text-4xl">☕️</p>
+            </div>
+            <p>=</p>
+            <div className="flex space-x-2 items-end">
+              <p className="font-semibold">1</p>
+              <p className="text-2xl text-muted-foreground font-semibold">
+                slot
+              </p>
+            </div>
+          </div>
+          <p className="text-xs text-muted-foreground mt-0.5">
+            * Make sure to use same email id which is used in this platform
+          </p>
+        </div>
+        <Button className="mt-2">Buy coffee</Button>
+      </DialogContent>
+    </Dialog>
+  );
+}
